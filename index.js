5dea9df9cf83350b4c061ad00eccadb6a2ac6312
Harsh Singh <sujeetsingh734321@gmail.com>
2023-06-06 13:53:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const connectDB = require("./config/database");
+connectDB();
+
+// allow express to uae json
+app.use(express.json());
+
+// hello world
+app.get("/", (req, res) => {
+  //   send simple json data
+  res.send({ message: "Hello World!" });
+});
+
+// create login routs
+app.use("/login", require("./routes/login"));
+app.use("/register", require("./routes/register"));
+app.use("/profile", require("./routes/profile"));
+
+// run server
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
