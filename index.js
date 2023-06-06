3d238326f115d37d29cb04cf1ddb32f76168aa3a
Harsh Singh <sujeetsingh734321@gmail.com>
2023-06-06 13:58:45 +0530

    hjj

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 // hello world
 app.get("/", (req, res) => {
   //   send simple json data
-  res.send({ message: "Hello World!" });
+  res.send({ message: "Election Target API" });
 });
 
 // create login routs
